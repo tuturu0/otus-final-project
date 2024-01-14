43e7ccb63750a8b04b7227863944e89160b5f807
tuturu0 <79896742+tuturu0@users.noreply.github.com>
2024-01-14 22:28:27 +0300

    Create test.sh

---
+++ search_engine_ui/tests/test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "testing.."
+if python -m unittest discover -s search_engine_ui/tests/ 2> >(grep -i OK)
+then 
+    echo "tests went fine!"
+else
+    echo "tests went wrong.."
+    exit 1
+fi
+echo "report:"
+if coverage run -m unittest discover -s search_engine_ui/tests/ 2> >(grep -i OK)
+then 
+    echo "app covered with tests!"
+else
+    echo "Report went wrong.."
+    exit 1
+fi
+coverage report --include search_engine_ui/ui/ui.py
